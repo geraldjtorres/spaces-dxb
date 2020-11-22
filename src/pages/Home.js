a7092cc5d845a6470b5738c3a04f4e652d5657d8
Gerald Torres <geraldjtorres@gmail.com>
2020-11-22 14:38:37 +0400

    added home styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import './Home.css'
 
 function Home() {
   return (
-    <div>
-      <p>home page</p>
+    <div className='Home'>
+      <div className='heading'>
+        <h1>Find spaces to work online in Dubai</h1>
+        <p>
+          Why work from your home or office when you can work from wherever you
+          want! Discover 40+ restaurants, cafés and co-working spaces across the
+          UAE.
+        </p>
+      </div>
     </div>
   )
 }
