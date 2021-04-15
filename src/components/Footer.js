39aa121724fc8bf5d6c0651e370825848ac01de6
Gerald Torres <geraldjtorres@gmail.com>
2021-04-15 19:18:58 +0400

    add stuff

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,10 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import LogoSmall from '../images/space-logo-small.svg'
 import './Footer.css'
+import './Header.css'
 
 function Footer() {
-  return <div className='main-footer'>hello</div>
+  return <div className='main-header'>hello</div>
 }
 
 export default Footer
