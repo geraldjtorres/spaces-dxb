131772cc91cda162400d64891d27b2a8c0386bf1
Gerald Torres <geraldjtorres@gmail.com>
2021-04-15 18:48:08 +0400

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,29 +1,10 @@
-import React from 'react';
-import './Footer.css';
-import { Link } from 'react-router-dom';
-import LogoSmall from '../images/space-logo-small.svg';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import LogoSmall from '../images/space-logo-small.svg'
+import './Footer.css'
 
 function Footer() {
-  return (
-    <div className='main-footer'>
-      <a href='#'>
-        <div className='footer-logo'>
-          <img src={LogoSmall} alt='' />
-        </div>
-      </a>
-      <div className='footer-nav'>
-        <ul className='footer-links'>
-          {/* <li className='footer-link'>
-            <Link to='/'>About</Link>
-          </li>
-          <li className='footer-link'>
-            <Link to='/'>Contact</Link>
-          </li> */}
-        </ul>
-        <span>Copyright &copy; 2020 Spaces.ae</span>
-      </div>
-    </div>
-  );
+  return <div className='main-footer'>hello</div>
 }
 
-export default Footer;
+export default Footer
