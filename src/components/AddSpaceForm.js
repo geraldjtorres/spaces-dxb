f81803085faa9b2585391c6c484f39f4378c1ece
Gerald Torres <geraldjtorres@gmail.com>
2021-04-15 18:02:29 +0400

    add error message and success message if adding a new space

--- src/components/AddSpaceForm.js
+++ src/components/AddSpaceForm.js
@@ -7,7 +7,7 @@ import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined'
 import usePlacesAutocomplete, { getDetails } from 'use-places-autocomplete'
 import axios from '../axios'
 import { useStateValue } from '../StateProvider'
-// import Spinner from './components/Spinner'
+import Spinner from '../components/Spinner'
 
 const AddSpaceForm = () => {
   const [
@@ -27,6 +27,8 @@ const AddSpaceForm = () => {
   const [isLoading, setLoading] = useState(false)
   const [place_id, setplaceId] = useState('')
   const [img, setImg] = useState('')
+  const [hasError, setHasError] = useState(false)
+  const [success, setSuccess] = useState(null)
 
   const {
     ready,
@@ -56,27 +58,34 @@ const AddSpaceForm = () => {
 
     setLoading(true)
 
-    const response = await axios({
-      method: 'POST',
-      url: '/spaces',
-      data: {
-        name: workspaceName,
-        website,
-        rating,
-        img,
-        socket: [socket],
-        category: [categoryType],
-        noise_level: [noiseLevel],
-        wifi_password: wifiPassword,
-        directions,
-        internet_speed: [internetSpeed],
-        place_id
-      }
-    })
+    try {
+      const response = await axios({
+        method: 'POST',
+        url: '/spaces',
+        data: {
+          name: workspaceName,
+          website,
+          rating,
+          img,
+          socket: [socket],
+          category: [categoryType],
+          noise_level: [noiseLevel],
+          wifi_password: wifiPassword,
+          directions,
+          internet_speed: [internetSpeed],
+          place_id
+        }
+      })
 
-    console.log('posted!', response)
+      console.log('posted!', response)
 
-    setLoading(false)
+      setTimeout(() => {
+        setLoading(false)
+        setSuccess(true)
+      }, 4000)
+    } catch (err) {
+      setHasError(true)
+    }
   }
 
   const handleInput = e => {
@@ -208,106 +217,122 @@ const AddSpaceForm = () => {
 
   return (
     <form className='form'>
-      <div className='input-style'>
-        <SearchIcon fontSize='small' />
+      {success ? (
+        <h5 className='feedback-message'>Thanks for submitting a space</h5>
+      ) : hasError ? (
+        <h5 className='feedback-message'>
+          Oops! Something went wrong. Please try again
+        </h5>
+      ) : (
+        <>
+          <div className='input-style'>
+            <SearchIcon fontSize='small' />
 
-        <input
-          value={value}
-          onChange={handleInput}
-          disabled={!ready}
-          placeholder='Find a Workspace'
-        />
-        {/* We can use the "status" to decide whether we should display the dropdown or not */}
-        {status === 'OK' && (
-          <ul className='suggestion-list'>{renderSuggestions()}</ul>
-        )}
-      </div>
-
-      <div className='form-group'>
-        <h5>Type of Space</h5>
-        <div className='radio-toolbar'>{categories && renderCategories()}</div>
-      </div>
-
-      <div className='form-group'>
-        <h5>Is there a Wi-Fi password?</h5>
-        <div className='radio-toolbar'>
-          <div className='radio-btn'>
             <input
-              type='radio'
-              id='passwordExistYes'
-              value='passwordExistYes'
-              onChange={e => setPasswordExist(e.target.value)}
-              checked={passwordExist === 'passwordExistYes'}
-              required
+              value={value}
+              onChange={handleInput}
+              disabled={!ready}
+              placeholder='Find a Workspace'
             />
-            <label htmlFor='passwordExistYes'>Yes</label>
+            {/* We can use the "status" to decide whether we should display the dropdown or not */}
+            {status === 'OK' && (
+              <ul className='suggestion-list'>{renderSuggestions()}</ul>
+            )}
           </div>
 
-          <div className='radio-btn'>
-            <input
-              type='radio'
-              id='passwordExistNo'
-              value='passwordExistNo'
-              onChange={e => setPasswordExist(e.target.value)}
-              checked={passwordExist === 'passwordExistNo'}
-              required
-            />
-            <label htmlFor='passwordExistNo'>No</label>
+          <div className='form-group'>
+            <h5>Type of Space</h5>
+            <div className='radio-toolbar'>
+              {categories && renderCategories()}
+            </div>
           </div>
-        </div>
 
-        {passwordExist === 'passwordExistYes' ? (
-          <div className='input-style wifiPass'>
-            <LockOpenIcon fontSize='small' />
-            <input
-              type='text'
-              value={wifiPassword}
-              onChange={e => setWifiPassword(e.target.value)}
-              placeholder='Wifi password'
-            />
+          <div className='form-group'>
+            <h5>Is there a Wi-Fi password?</h5>
+            <div className='radio-toolbar'>
+              <div className='radio-btn'>
+                <input
+                  type='radio'
+                  id='passwordExistYes'
+                  value='passwordExistYes'
+                  onChange={e => setPasswordExist(e.target.value)}
+                  checked={passwordExist === 'passwordExistYes'}
+                  required
+                />
+                <label htmlFor='passwordExistYes'>Yes</label>
+              </div>
+
+              <div className='radio-btn'>
+                <input
+                  type='radio'
+                  id='passwordExistNo'
+                  value='passwordExistNo'
+                  onChange={e => setPasswordExist(e.target.value)}
+                  checked={passwordExist === 'passwordExistNo'}
+                  required
+                />
+                <label htmlFor='passwordExistNo'>No</label>
+              </div>
+            </div>
+
+            {passwordExist === 'passwordExistYes' ? (
+              <div className='input-style wifiPass'>
+                <LockOpenIcon fontSize='small' />
+                <input
+                  type='text'
+                  value={wifiPassword}
+                  onChange={e => setWifiPassword(e.target.value)}
+                  placeholder='Wifi password'
+                />
+              </div>
+            ) : null}
           </div>
-        ) : null}
-      </div>
 
-      <div className='form-group'>
-        <h5>Internet Speed</h5>
-        <div className='radio-toolbar'>
-          {internetSpeeds && renderInternetSpeeds()}
-        </div>
-      </div>
+          <div className='form-group'>
+            <h5>Internet Speed</h5>
+            <div className='radio-toolbar'>
+              {internetSpeeds && renderInternetSpeeds()}
+            </div>
+          </div>
 
-      <div className='form-group'>
-        <h5>Are there plug sockets?</h5>
-        <div className='radio-toolbar'>{sockets && renderSockets()}</div>
-      </div>
+          <div className='form-group'>
+            <h5>Are there plug sockets?</h5>
+            <div className='radio-toolbar'>{sockets && renderSockets()}</div>
+          </div>
 
-      <div className='form-group'>
-        <h5>What is the noise level?</h5>
-        <div className='radio-toolbar'>
-          {noiseLevels && renderNoiseLevels()}
-        </div>
-      </div>
+          <div className='form-group'>
+            <h5>What is the noise level?</h5>
+            <div className='radio-toolbar'>
+              {noiseLevels && renderNoiseLevels()}
+            </div>
+          </div>
 
-      <button
-        className='add-space-btn'
-        disabled={
-          !value ||
-          !categoryType ||
-          !passwordExist ||
-          !internetSpeed ||
-          !sockets ||
-          !noiseLevel ||
-          !rating ||
-          !place_id ||
-          !website ||
-          !directions
-            ? true
-            : false
-        }
-        onClick={e => onHandleSubmit(e)}
-      >
-        Add a space
-      </button>
+          {isLoading ? (
+            <Spinner />
+          ) : (
+            <button
+              className='add-space-btn'
+              disabled={
+                !value ||
+                !categoryType ||
+                !passwordExist ||
+                !internetSpeed ||
+                !sockets ||
+                !noiseLevel ||
+                !rating ||
+                !place_id ||
+                !website ||
+                !directions
+                  ? true
+                  : false
+              }
+              onClick={e => onHandleSubmit(e)}
+            >
+              Add a space
+            </button>
+          )}
+        </>
+      )}
     </form>
   )
 }
